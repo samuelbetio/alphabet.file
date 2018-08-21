6b56cd1bb5b3b9e071c585591f3a67dc1e9940e1
Pearlton Namacpacan <34736139+Pearlton@users.noreply.github.com>
2018-08-21 11:18:32 +0800

    The Fitness Trend

---
+++ A/B/C/D/10/assets/js/custom.js
@@ -0,0 +1,105 @@
+/*global $:false, jQuery:false, console:false */
+(function ($) {
+"use strict";
+    
+    //Google Map
+    var get_latitude = $('#google-map').data('latitude');
+    var get_longitude = $('#google-map').data('longitude');
+
+    function initialize_google_map() {
+        var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
+        var mapOptions = {
+            zoom: 14,
+            scrollwheel: false,
+            center: myLatlng
+        };
+        var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize_google_map);
+    
+	//scroll to top
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollup').fadeIn();
+			} else {
+			$('.scrollup').fadeOut();
+		}
+	});
+	$('.scrollup').click(function(){
+		$("html, body").animate({ scrollTop: 0 }, 600);
+			return false;
+	});
+
+    $('.accordion').on('show', function (e) {
+	
+		$(e.target).prev('.accordion-heading').find('.accordion-toggle').addClass('active');
+		$(e.target).prev('.accordion-heading').find('.accordion-toggle i').removeClass('icon-plus');
+		$(e.target).prev('.accordion-heading').find('.accordion-toggle i').addClass('icon-minus');
+    });
+    
+    $('.accordion').on('hide', function (e) {
+        $(this).find('.accordion-toggle').not($(e.target)).removeClass('active');
+		$(this).find('.accordion-toggle i').not($(e.target)).removeClass('icon-minus');
+		$(this).find('.accordion-toggle i').not($(e.target)).addClass('icon-plus');
+    });	
+
+     
+	//navigation
+	$('.navigation').onePageNav({
+		begin: function() {
+			console.log('start');
+		},
+		end: function() {
+			console.log('stop');
+		},
+			scrollOffset: 0		
+	});
+	
+		// fancybox
+		$(".fancybox").fancybox({				
+				padding : 0,
+				autoResize: true,
+				beforeShow: function () {
+					this.title = $(this.element).attr('title');
+					this.title = '<h4>' + this.title + '</h4>' + '<p>' + $(this.element).parent().find('img').attr('alt') + '</p>';
+				},
+				helpers : {
+					title : { type: 'inside' },
+				}
+			});
+			
+		$('.fancybox-media').fancybox({
+			openEffect  : 'none',
+			closeEffect : 'none',
+			helpers : {
+				media : {}
+			}
+		});
+		
+	//nicescroll
+	$("html").niceScroll({zindex:999,cursorborder:"",cursorborderradius:"2px",cursorcolor:"#191919",cursoropacitymin:.5});
+	function initNice() {
+		if($(window).innerWidth() <= 960) {
+			$('html').niceScroll().remove();
+		} else {
+			$("html").niceScroll({zindex:999,cursorborder:"",cursorborderradius:"2px",cursorcolor:"#191919",cursoropacitymin:.5});
+		}
+	}
+	$(window).load(initNice);
+	$(window).resize(initNice);
+
+})(jQuery);
+
+$(window).scroll(function(){
+"use strict";
+	if($(window).scrollTop()<10){
+		
+		$('.fade').stop(true,true).fadeTo("slow",1);
+	} else {
+	$('.fade').stop(true,true).fadeTo("slow", 0.33);
+	}
+});
---
+++ A/B/C/D/10/readme.txt
@@ -0,0 +1,6 @@
+Thanks for downloading this theme!
+
+Theme Name: Groovin
+Theme URL: https://bootstrapmade.com/groovin-free-bootstrap-theme/
+Author: BootstrapMade
+Author URL: https://bootstrapmade.com
