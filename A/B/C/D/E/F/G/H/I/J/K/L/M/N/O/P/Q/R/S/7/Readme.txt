4237ec27e2b183eaac6294a286830a0f4da4d197
Pearlton Namacpacan <34736139+Pearlton@users.noreply.github.com>
2018-08-21 11:20:23 +0800

    Laura Thomson

---
+++ A/B/C/D/E/F/G/H/I/J/K/L/M/N/O/P/Q/R/S/7/Readme.txt
@@ -0,0 +1,6 @@
+Thanks for downloading this theme!
+
+Theme Name: Laura
+Theme URL: https://bootstrapmade.com/laura-free-creative-bootstrap-theme/
+Author: BootstrapMade.com
+Author URL: https://bootstrapmade.com
---
+++ A/B/C/D/E/F/G/H/I/J/K/L/M/N/O/P/Q/R/S/7/js/custom.js
@@ -0,0 +1,51 @@
+(function ($) {
+    
+    // Init Wow
+    wow = new WOW( {
+        animateClass: 'animated',
+        offset:       100
+    });
+    wow.init();
+    
+    // Navigation scrolls
+    $('.navbar-nav li a').bind('click', function(event) {
+        $('.navbar-nav li').removeClass('active');
+        $(this).closest('li').addClass('active');
+        var $anchor = $(this);
+        var nav = $($anchor.attr('href'));
+        if (nav.length) {
+        $('html, body').stop().animate({				
+            scrollTop: $($anchor.attr('href')).offset().top				
+        }, 1500, 'easeInOutExpo');
+        
+        event.preventDefault();
+        }
+    });
+    
+    // About section scroll
+    $(".overlay-detail a").on('click', function(event) {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 900, function(){
+            window.location.hash = hash;
+        });
+    });
+       
+    //jQuery to collapse the navbar on scroll
+    $(window).scroll(function() {
+        if ($(".navbar-default").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+    
+    // Testimonials Slider
+    $('.bxslider').bxSlider({
+      adaptiveHeight: true,
+      mode: 'fade'
+    });
+    
+})(jQuery);
