4c02fc7ff1de17fd44bac0999ae8753758618885
Frances Maffy Valor <40848973+FrancesMaffyValor@users.noreply.github.com>
2018-08-25 15:39:24 +0800

    Add files via upload

---
+++ A/B/C/D/E/F/G/H/I/J/K/L/M/9/Readme.txt
@@ -0,0 +1,6 @@
+Thanks for downloading this theme!
+
+Theme Name: MyBiz
+Theme URL: https://bootstrapmade.com/mybiz-free-business-bootstrap-theme/
+Author: BootstrapMade.com
+Author URL: https://bootstrapmade.com
---
+++ A/B/C/D/E/F/G/H/I/J/K/L/M/9/js/script.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+
+	//Smooth Scroll
+	$(function() {
+		$('a[href*="#"]:not([href="#"])').click(function() {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html, body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+	});
+
+
+	// Main Menu
+	$('#main-nav').affix({
+		offset: {
+			top: $('header').height()
+		}
+	});
+
+
+	// Top Search
+	$("#ss").click(function(e) {
+		e.preventDefault();
+		$(this).toggleClass('current');
+		$(".search-form").toggleClass('visible');
+	});
+
+
+	//Slider
+	$('.flexslider').flexslider({
+		animation: "fade",
+		directionNav: false,
+		pauseOnAction: false,
+	});
+
+	var containerPosition = $('.container').offset();
+	var positionPad = containerPosition.left + 15;
+
+	$('#slider').find('.caption').css({
+		left: positionPad + 'px',
+		marginTop: '-' + $('.caption').height() / 2 + 'px'
+	});
+
+
+	// number effect
+	$('.about-bg-heading').one('inview', function(event, visible) {
+		if (visible == true) {
+			$('.count').each(function() {
+				$(this).prop('Counter', 0).animate({
+					Counter: $(this).text()
+				}, {
+					duration: 5000,
+					easing: 'swing',
+					step: function(now) {
+						$(this).text(Math.ceil(now));
+					}
+				});
+			});
+		}
+	});
+
+
+	//Google Map
+    var get_latitude = $('#google-map').data('latitude');
+    var get_longitude = $('#google-map').data('longitude');
+
+    function initialize_google_map() {
+        var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
+        var mapOptions = {
+            zoom: 14,
+            scrollwheel: false,
+            center: myLatlng
+        };
+        var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize_google_map);
+
+});
