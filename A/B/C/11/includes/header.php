c3328cc2ba251ccb1ce2589fa5c679cc52e7aced
Monique Ramree Mae Basalan <43129028+monicbasalan@users.noreply.github.com>
2018-09-10 16:13:39 +0800

    Add files via upload

---
+++ A/B/C/11/includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en"<?=$manifest?>>
+<head>
+<meta charset=utf-8>
+<meta name="viewport" content="width=device-width">
+<title>HTML5 Demo: <?=$title?></title>
+<link rel="stylesheet" href="/css/html5demos.css">
+<?php if (!$manifest) : ?><script src="/js/h5utils.js"></script><?php endif ?>
+</head>
+<body>
+<section id="wrapper">
+<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=html5demoscom" id="_carbonads_js"></script>
+    <header>
+      <h1><?=$title?></h1>
+    </header>
